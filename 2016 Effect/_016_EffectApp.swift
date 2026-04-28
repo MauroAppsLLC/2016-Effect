085c86879fe18a54e00b475ac6188f3708924d89
Mark Mauro <markymauro@gmail.com>
2026-04-28 15:13:01 -0400

    Initial Commit

---
+++ 2016 Effect/_016_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _016_EffectApp.swift
+//  2016 Effect
+//
+//  Created by Mark Mauro on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct _016_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
